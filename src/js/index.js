6e1e9cd27b67b9b3d10cc317a155330b2f3a78a5
jackie <lianghengwei2011@163.com>
2020-10-06 05:48:41 +0800

    [issue] update personal info and Optimization. [issue number] [commit] Jackie lianghengwei2011@163.com [reviewer]

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,25 @@
-let rangeLists = document.querySelectorAll('.range')
-
-rangeLists.forEach(function (element) {
-  let R = Math.random() * 255
-  let G = Math.random() * 255
-  let B = Math.random() * 255
-  setInterval(() => {
-    R += Math.random() * 6
-    G += Math.random() * 6
-    B += Math.random() * 6
-    if (R > 255) {
-      R = Math.random() * 255
-    }
-    if (G > 255) {
-      G = Math.random() * 255
-    }
-    if (B > 255) {
-      B = Math.random() * 255
-    }
-    element.style.backgroundColor = `rgb(${R},${G},${B})`
-  }, 200);
-});
-
 let infoSec = document.querySelector(".info")
 let transY, scrollInfoId;
-transY = 0
+transY = 0;
+const intervalTime = 1000;
+const translateYSteps = 20;
 let slideUp = () => {
-  transY += 20
+  transY += translateYSteps;
   if (infoSec.offsetHeight < transY) {
-    // clearInterval(scrollInfoId)
-    infoSec.style.transform = `translateY(-${infoSec.offsetHeight}px)`
-    transY = 0
+    infoSec.style.transform = `translateY(-${infoSec.offsetHeight}px)`;
+    transY = 0;
   }
-  infoSec.style.transform = `translateY(-${transY}px)`
+  infoSec.style.transform = `translateY(-${transY}px)`;
 }
 scrollInfoId = setInterval(() => {
-  slideUp()
-}, 600);
+  slideUp();
+}, intervalTime);
 
-infoSec.addEventListener('mouseenter', function (event) {
-  clearInterval(scrollInfoId)
-})
+infoSec.addEventListener('mouseover', function (event) {
+  clearInterval(scrollInfoId);
+});
 infoSec.addEventListener('mouseleave', function (event) {
   scrollInfoId = setInterval(() => {
-    slideUp()
-  }, 600);
-})
+    slideUp();
+  }, intervalTime);
+});
